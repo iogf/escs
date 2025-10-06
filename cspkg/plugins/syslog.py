c4318e91bfe4bcd8bf9f608d6629ec9750471071
Iury O. G. Figueiredo <last.src@gmail.com>
2025-10-06 13:40:07 -0300

    Syslog improvements. Option to log stdout into a xstr instance.

--- cspkg/plugins/syslog.py
+++ cspkg/plugins/syslog.py
@@ -3,6 +3,7 @@ from cspkg.core import Namespace, Main, Plugin
 from cspkg.start import root
 import sys
 
+
 class SyslogNS(Namespace):
     pass
 
@@ -10,24 +11,57 @@ class LogWrapper:
     """
     """
 
-    def __init__(self, stdout, logwin):
-        self.logwin = logwin
-        self.stdout = stdout
+    def __init__(self, xstr=None):
+        self.xwin = TextWindow('', title='Cmd Output')
+        self.xwin.withdraw()
+        
+        self.xstr = xstr
 
     def write(self, data):
-        self.logwin.text.insert('end', data)
-        self.logwin.text.see('end')
-        self.stdout.write(data)
+        self.xwin.text.insert('end', data)
+        self.xwin.text.see('end')
+        sys.__stdout__.write(data)
+
+        if self.xstr is not None:
+            self.update_xstr(data)
+
+    def update_xstr(self, data):
+        self.xstr.insert('end', data)
+        self.xstr.see('insert')
+
+    def set_xstr(self, xstr):
+        self.xstr = xstr
+
+    def unset_xstr(self):
+        self.xstr = None
+
+    def flush(self):
+        pass
+
+logwrapper = LogWrapper()
+sys.stdout = logwrapper
 
 class Syslog(Plugin):
+
     def __init__(self, xstr):
         super().__init__(xstr)
-        self.add_kmap(SyslogNS, Main, 
-        '<Alt-q>', lambda event: logwin.display())
 
-logwin = TextWindow('', title='Cmd Output')
-logwin.withdraw()
-logwrapper = LogWrapper(sys.__stdout__, logwin)
-sys.stdout = logwrapper
+        self.add_kmap(SyslogNS, Main, 
+        '<Alt-q>', lambda event: 
+        logwrapper.xwin.display())
+
+        self.add_kmap(SyslogNS, Main, 
+        '<Control-q>', self.add_chan)
+
+        self.add_kmap(SyslogNS, Main, 
+        '<Control-Q>', self.rm_chan)
+
+    def add_chan(self, chan):
+        logwrapper.set_xstr(self.xstr)
+        root.status.set_msg('Output set: %s' % self.xstr.index('insert'))
+
+    def rm_chan(self, chan):
+        logwrapper.unset_xstr()
+        root.status.set_msg('Output removed!')
 
 install = Syslog
