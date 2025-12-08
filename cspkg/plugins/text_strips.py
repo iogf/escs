5c21eff9b375355e64f6d19a7ffd50a4439d6f4a
Iury O. G. Figueiredo <last.src@gmail.com>
2025-12-08 01:56:52 -0300

    Fixing bug due to renaming Xstr.tag_xsub.

--- cspkg/plugins/text_strips.py
+++ cspkg/plugins/text_strips.py
@@ -3,20 +3,20 @@ from cspkg.core import Command
 from re import escape
 
 @Command()
-def strip(area, chars=' '):
+def strip(xstr, chars=' '):
     """
     Strip chars off the beginning of all selected lines.
     if chars is not given it removes spaces.
     """
 
-    area.tag_xsub('sel', '^[%s]+' % escape(chars), '')
+    xstr.replace_ranges('sel', '^[%s]+' % escape(chars), '')
 
 @Command()
-def rstrip(area, chars=' '):
+def rstrip(xstr, chars=' '):
     """
     Strip chars off the end of all selected lines.
     if chars is not given it removes spaces.
     """
 
-    area.tag_xsub('sel', '[%s]+$' % escape(chars), '')
+    xstr.replace_ranges('sel', '[%s]+$' % escape(chars), '')
 
