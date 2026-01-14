f4e0c7fc01a85fd58a81a7dcda3a34134b842de5
Iury O. G. Figueiredo <last.src@gmail.com>
2026-01-14 15:16:32 -0300

    Fixing tests.

--- tests/test_xstr.py
+++ tests/test_xstr.py
@@ -1,17 +1,18 @@
 from cspkg.plugins.normal_mode import Normal
-from cspkg.core import Main, Mode
+from cspkg.core import Main, Mode, EscsApp
 from cspkg.start import root
 import random
 import unittest
 import time
 
-class TestXstr(unittest.TestCase):
+class TestXstr0(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         pass
 
     @classmethod
     def tearDownClass(cls):
+        root.destroy()
         pass
 
     def test0(self):
@@ -55,53 +56,6 @@ class TestXstr(unittest.TestCase):
         self.assertEqual(index2, '5.4')
 
         pass
-
-    """
-    def test2(self):
-        xstr0 = self.xstr.master.master.create('Xstr0')
-        xstr1 = self.xstr.master.master.create('Xstr1')
-
-        xstr2 = self.xstr.master.master.master.create('Xstr2')
-
-        xstr3 = xstr2.master.master.create('Xstr3')
-        lst0 = list(self.xstr.xstr_widgets(root))
-
-        self.assertEqual(len(lst0), 5)
-
-        self.assertIn(xstr0, lst0)
-        self.assertIn(xstr1, lst0)
-        self.assertIn(xstr2, lst0)
-        self.assertIn(xstr3, lst0)
-
-        # Create tabs and check if if Xstr.xstr_widgets returns
-        # the correct Xstr instances.
-        xstr4 = root.note.create('Xstr4')
-        xstr5 = root.note.create('Xstr5')
-
-        lst1 = list(self.xstr.xstr_widgets(root))
-        self.assertIn(xstr4, lst1)
-        self.assertIn(xstr5, lst1)
-        self.assertEqual(len(lst1), 7)
-
-        lst2 = self.xstr.get_opened_files(root)
-        self.assertEqual(len(lst1), 7)
-
-        for indi, indj in lst2.items():
-            self.assertIn(indj, lst1)
-
-        xstr0.master.destroy()
-        xstr1.master.destroy()
-
-        xstr2.master.master.destroy()
-        xstr3.master.destroy()
-        
-        xstr4.master.master.master.destroy()
-        xstr5.master.master.master.destroy()
-
-        lst3 = list(self.xstr.xstr_widgets(root))
-        self.assertEqual(len(lst3), 1)
-        self.assertIn(self.xstr, lst3)
-    """
 
     def test3(self):
         """
@@ -185,6 +139,64 @@ class TestXstr(unittest.TestCase):
         (xstr.index('2.0 lineend -4c'), xstr.index('2.0 lineend')))
 
         pass
+
+
+class TestXstr1(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.root = EscsApp()
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.root.destroy()
+        pass
+
+    def test0(self):
+        xstr = self.root.note.create('Tests')
+        xstr0 = xstr.master.master.create('Xstr0')
+        xstr1 = xstr.master.master.create('Xstr1')
+
+        xstr2 = xstr.master.master.master.create('Xstr2')
+
+        xstr3 = xstr2.master.master.create('Xstr3')
+        lst0 = list(xstr.xstr_widgets(self.root))
+
+        self.assertEqual(len(lst0), 5)
+
+        self.assertIn(xstr0, lst0)
+        self.assertIn(xstr1, lst0)
+        self.assertIn(xstr2, lst0)
+        self.assertIn(xstr3, lst0)
+
+        # Create tabs and check if if Xstr.xstr_widgets returns
+        # the correct Xstr instances.
+        xstr4 = self.root.note.create('Xstr4')
+        xstr5 = self.root.note.create('Xstr5')
+
+        lst1 = list(xstr.xstr_widgets(self.root))
+        self.assertIn(xstr4, lst1)
+        self.assertIn(xstr5, lst1)
+        self.assertEqual(len(lst1), 7)
+
+        lst2 = xstr.get_opened_files(self.root)
+        self.assertEqual(len(lst1), 7)
+
+        for indi, indj in lst2.items():
+            self.assertIn(indj, lst1)
+
+        xstr0.master.destroy()
+        xstr1.master.destroy()
+
+        xstr2.master.master.destroy()
+        xstr3.master.destroy()
+        
+        xstr4.master.master.master.destroy()
+        xstr5.master.master.master.destroy()
+
+        lst3 = list(xstr.xstr_widgets(self.root))
+        self.assertEqual(len(lst3), 1)
+        self.assertIn(xstr, lst3)
 
 if __name__ == '__main__':
     unittest.main()
