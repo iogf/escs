53bd9c5e9ef6b846b2cff8755577fd6b400c8e82
Iury O. G. Figueiredo <last.src@gmail.com>
2026-01-18 22:32:20 -0300

    Adding tests.

---
+++ tests/test_extra_mode.py
@@ -0,0 +1,35 @@
+from cspkg.plugins.extra_mode import Extra, ExtraModeNS, ExtraMode
+from cspkg.plugins.normal_mode import Normal, NormalModeNS, NormalMode
+from cspkg.core import rcmod
+from cspkg.core import Mode
+from cspkg.start import root
+from tkinter import TclError
+import unittest
+
+class TestExtraMode(unittest.TestCase):
+    @classmethod
+    def tearDownClass(cls):
+        root.destroy()
+
+    def test0(self):
+        xstr = root.note.create('Tests')
+        mod0 = ExtraMode(xstr)
+        mod1 = NormalMode(xstr)
+        
+        xstr.insert('end', 'ExtraMode plugin test.\n')
+        root.note.select(xstr.master.master.master)
+        xstr.focus_set()
+        root.update() 
+
+        xstr.event_generate('<Alt-v>')
+        self.assertEqual(mod0.mode, Extra)
+
+        xstr.tag_add('sel', '1.0', 'end')
+        xstr.event_generate('<Escape>')
+        self.assertEqual(xstr.tag_nextrange('sel', '1.0'), ())
+        self.assertEqual(mod1.mode, Normal)
+        
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
