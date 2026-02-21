346453294791d0c64ae31648f64078bee20843a2
Iury O. G. Figueiredo <last.src@gmail.com>
2026-02-21 15:45:38 -0300

    Adding mechanism to clear statusbar.

--- cspkg/plugins/normal_mode.py
+++ cspkg/plugins/normal_mode.py
@@ -1,4 +1,5 @@
 from cspkg.core import Namespace, Mode, Main, Plugin
+from cspkg.start import root
 
 class NormalModeNS(Namespace):
     pass
@@ -19,6 +20,7 @@ class NormalMode(Plugin):
         """
         """
         self.xstr.tag_remove('sel', '1.0', 'end')
+        root.status.clear_msg()
         self.chmode(Normal)
 
 install = NormalMode
