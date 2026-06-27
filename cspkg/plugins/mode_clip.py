8b5bdf4b0d9ca301e96c34fa96510d8b719da232
Iury O. G. Figueiredo <last.src@gmail.com>
2026-06-27 09:52:34 -0300

    Changing plugin name.

--- cspkg/plugins/mode_clip.py
+++ cspkg/plugins/mode_clip.py
@@ -2,32 +2,31 @@ from cspkg.core import Namespace, Main, Plugin, Mode
 from cspkg.plugins.normal_mode import Normal
 from cspkg.start import root
 
-class QModeNS(Namespace):
+class ModeClipNS(Namespace):
     pass
 
-class QMode(Plugin):
-    pinned_mode = None
+class ModeClip(Plugin):
+    mode_ref = None
     def __init__(self, xstr):
         super().__init__(xstr)
-        self.add_kmap(QModeNS, Main, 
+        self.add_kmap(ModeClipNS, Main, 
         '<Control-b>', self.pin_mode)
 
-        self.add_kmap(QModeNS, Normal, 
+        self.add_kmap(ModeClipNS, Normal, 
         '<Key-b>', self.unpin_mode)
 
     def pin_mode(self, event):
         taglist = self.xstr.bindtags()
-        self.pinned_mode = taglist[1]
-        root.status.set_msg('QMode - Pinned mode.')
+        self.mode_ref = taglist[1]
+        root.status.set_msg('ModeClip - Pinned mode.')
         self.chmode(Normal)
 
     def unpin_mode(self, event):
         taglist = self.xstr.bindtags()
 
         self.xstr.bindtags((taglist[0], 
-        self.pinned_mode, taglist[2]))
+        self.mode_ref if self.mode_ref else taglist[1], taglist[2]))
         self.xstr.event_generate('<<Chmode>>')
-        root.status.set_msg('QMode - Unpinned mode.')
+        root.status.set_msg('ModeClip - Unpinned mode.')
 
-install = QMode
-
+install = ModeClip
